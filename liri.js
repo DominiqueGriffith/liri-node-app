8a92746ccc73191c6460324730a07c9215532d58
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-06-16 16:48:53 -0400

    -Added Modules for Axios, spotify, moment and DotEnv -Hid API key -Parsed information to make an API call for concert info using Axios (bandsintown)

--- liri.js
+++ liri.js
@@ -1,17 +1,70 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
+var axios = require('axios');
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+var parameter = process.argv[3];
+var artist = "";
 
-var spotify = new Spotify({
-    // 669e9411abe241ed80fa2f8663dc0da6,
-    // 4b9732c618624196b7460f6b01098607
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret>
-});
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+  
+
+function switchCase() {
+  switch (command) {
+    case "concert-this":
+      break;
+
+    default:
+      break;
   }
+}
+function bandsInTown(parameter) {
+  if ('concert-this') {
+  
+    for (var i = 3; i < process.argv.length; i++) {
+      artist += process.argv[i];
+    }
 
-console.log(data); 
-});
+
+  }
+  // console.log("Artist " + artist);
+
+}
+switchCase();
+bandsInTown();
+
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+  .then(function (response) {
+
+var concertInfo = response.data;
+
+  //  concertInfo = {
+  //     datetime: "",
+  //     venue: "",
+  //     datetime: "",
+
+  //   }
+    // handle success
+    console.log(concertInfo);
+  }) 
+  .catch(function (error) {
+    // handle error
+    // console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+
+
+
+
+
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+
+// console.log(data); 
+// });
